c235504037901ab8af627aade4f602c2507ae6d7
unknown <raidislam>
2024-12-19 01:47:09 +0600

    update

---
+++ src/app/components/services.tsx
@@ -0,0 +1,28 @@
+import Accordion from "./accordion";
+import Container from "./container";
+
+export default function Services() {
+  return (
+    <section className="bg-white py-[100px]">
+      <Container>
+        <div className="flex flex-col items-center">
+          <h2 className="leading-none text-center">
+            <span className="text-7xl xl:text-6xl font-bold text-[#0C111F]">
+              The Services We Offer
+            </span>
+            <span className="ml-10 text-7xl xl:text-6xl font-bold bg-gradient-to-r from-[#9B9FFF] to-[#666CFF] bg-clip-text text-transparent">
+              For You
+            </span>
+          </h2>
+          <p className="font-inter font-medium text-[20px] leading-[28px]  text-[#B6B8BC] w-[745px] text-center">
+            As a top software company, we specialize in creating easy-to-use
+            websites, apps, and captivating visuals
+          </p>
+        </div>
+        <div>
+          <Accordion />
+        </div>
+      </Container>
+    </section>
+  );
+}
